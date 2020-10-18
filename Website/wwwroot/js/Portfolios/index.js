c5525f557e4fb11308dbc21ef0d53c92ed6c55e1
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2020-10-18 16:13:12 +0100

    Got datatable working with buttons

--- Website/wwwroot/js/Portfolios/index.js
+++ Website/wwwroot/js/Portfolios/index.js
@@ -6,27 +6,38 @@
             url: "../api/Portfolio/GetMyPortfolios/",
             dataSrc: ''
         },
-        "columns": [
+        columns: [
             { "data": "name" },
             { "data": "numberOfProperties" },
             { "data": "grossIncome" },
             { "data": "createdDate" },
             { "data": "updatedDate" },
-        ],
-        columnDefs: [{
-            // Format dates...
-            targets: [3, 4], render: function (data) {
-                return moment(data).format('llll');
+            {
+                "data": "Actions",
+                sortable: false,
+                "mRender": function (data, type, row) {
+                    return '<a href=/Portfolio/Edit?id=' + row.id + '><span class="material-icons">edit</span ></a>' + '  ' + 
+                        '<a href=/Portfolio/Delete?id=' + row.id + '><span class="material-icons" style="color:red">delete</span ></a>';
+                }
             }
-        }]
+        ],
+        columnDefs: [
+            {
+                // Format dates...
+                "targets": [3, 4],
+                "render": function (data) {
+                    return moment(data).format('llll');
+                }
+            }
+        ]
     });
 
+    // Handle row clicked
     $('#portfolioIndexTable tbody').on('click', 'tr', function () {
-        var loc = window.location;
         var row = table.row($(this)).data();
         var url = "/Portfolio/Details?id=" + row.id; 
         window.location.href = url;
-
     });
+
 });
 
